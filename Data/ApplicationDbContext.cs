bb9c87666cdf2c70e2dc4118d9fc82af5f66bedc
Sajid Khan <khanalex301@gmail.com>
2025-04-09 06:48:18 +0300

    implement it in practice.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManager.Data;
+
+
+public class ApplicationDbContext : IdentityDbContext
+{
+	public ApplicationDbContext(DbContextOptions options) : base(options)
+	{
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder options) => 
+	options.UseSqlServer("Server=NCCTCC;initial catalog=iii;Trusted_Connection=True;multipleactiveresultsets=true;TrustServerCertificate=True");
+}
